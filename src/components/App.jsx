ef38d796882f156db30d7fb55990c4e4661b81c1
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2023-04-27 13:23:02 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: 'lightgray',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React template
+    </div>
+  );
+};
